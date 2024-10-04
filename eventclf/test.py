b6d20d5645182a1bd9bf1ca2f067f7b09555fcdd
Nonie Alexander <nonie.alexander@faculty.ai>
2024-10-04 10:22:33 +0000

    testing pre-commit

---
+++ eventclf/test.py
@@ -0,0 +1,1 @@
+new_string = "this is a very long string to check that the pre-commit is working how i want"
---
+++ strava/test.py
@@ -0,0 +1,3 @@
+new_string = (
+    "this is a very long string to check that the pre-commit is working how i want"
+)
